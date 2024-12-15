3e866468a7e77567ced0745676b5b0730f30b819
Christopher Willians <163358050+christopherfifo@users.noreply.github.com>
2024-12-15 14:25:05 -0300

    scaner login e cadastro

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,23 @@ btn_menu.forEach(button => {
         menu.classList.toggle('menu-active');
     });
 });
+
+
+const btn_caixa = document.querySelector('.caixinha');
+const password = document.querySelector('.senha');
+
+btn_caixa.addEventListener('change', function() {
+    if (btn_caixa.checked) {
+        password.type = 'text';
+    } else {
+        password.type = 'password';
+    }
+});
+
+const btn_login = document.querySelector('.btn_login');
+
+btn_login.addEventListener('click', function() {
+    window.location.href = 'pages/home.html';
+});
+
+
